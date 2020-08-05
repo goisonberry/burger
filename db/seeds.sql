d0db0c7c622a81b765a89ed1b87018519fe538ca
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-08-05 13:36:41 -0700

    redid seeds file to make sure db was working well. confirmed.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', TRUE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('KFC Fillet Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Maccies Burger', TRUE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Supreme Burger', TRUE);
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Green Chile Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
