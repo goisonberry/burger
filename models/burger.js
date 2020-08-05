3c62d22c23fce15ed6206ceb7d0448bf925e5bb6
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-08-05 13:47:31 -0700

    required orm. redid code to create, update. code working now.

--- models/burger.js
+++ models/burger.js
@@ -1,25 +1,24 @@
-const orm = require("../config/orm.js");
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-const burger = {
+var burger = {
   all: function (cb) {
     orm.all("burgers", function (res) {
       cb(res);
     });
   },
-  create: function (name, cb) {
-    orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
+  // The variables cols and vals are arrays.
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
   },
-  update: function (id, cb) {
-    const condition = "id=" + id;
-    orm.update(
-      "burgers",
-      {
-        devoured: true,
-      },
-      condition,
-      cb
-    );
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
   },
 };
 
+// Export the database functions for the controller (catsController.js).
 module.exports = burger;
