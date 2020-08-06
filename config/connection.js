ed3bb4b43394a153f081801117cbd6c3535d6813
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-08-05 19:29:52 -0700

    typo. should fix issue.

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ var mysql = require("mysql");
 require("dotenv").config();
 var connection;
 
-if ((process.env.JAWSDB_URL {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
+} else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "TheC@micGuy1985",
-  database: "burgers_db",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "TheC@micGuy1985",
+    database: "burgers_db",
   });
-};
+}
 
 // Make connection.
 connection.connect(function (err) {
